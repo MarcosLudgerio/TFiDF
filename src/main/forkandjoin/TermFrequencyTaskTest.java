7df75894c1fb943e2c8e9fc36f2e9609ea85a577
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2024-11-09 21:13:02 -0300

    feat: tansform project in a maven project

--- src/main/forkandjoin/TermFrequencyTaskTest.java
+++ src/main/forkandjoin/TermFrequencyTaskTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.logging.Logger;
 
 public class TermFrequencyTaskTest {
-    private static final String filePath = "D:\\repositorios-git\\projetos-diversos\\TFiDF\\datasets\\dataset_100mb.txt";
+    private static final String filePath = "D:\\repositorios-git\\TFiDF-kotlin\\TFiDF vk\\dataset\\dataset_test.txt";
     public static final Logger LOGGER = Logger.getLogger(TermFrequencyTask.class.getName());
 
     public static void main(String[] args) {
@@ -19,17 +19,17 @@ public class TermFrequencyTaskTest {
             LOGGER.info("Criamos um array com: " + valuesArray.length + " elementos");
             ForkJoinPool pool = new ForkJoinPool();
             TermFrequencyTask taskFrequencyTerm = new TermFrequencyTask(valuesArray, 0, listOfLines.size());
+            pool.invoke(taskFrequencyTerm);
 
-            List<Map<String, Integer>> totalTermFrequency = pool.invoke(taskFrequencyTerm);
-
-            InverseDocumentFrequencyTask inverseDocumentFrequencyTask = new InverseDocumentFrequencyTask(totalTermFrequency, totalTermFrequency.size());
-
-            Map<String, Double> inverseTermFrequency = pool.invoke(inverseDocumentFrequencyTask);
-
-            TFiDFTask tFiDFTask = new TFiDFTask(totalTermFrequency, inverseTermFrequency);
-            pool.invoke(tFiDFTask);
-            LOGGER.info("Term frequency task completed");
-            List<Map<String, Double>> listTfiDf = pool.invoke(tFiDFTask);
+//            List<Map<String, Integer>> totalTermFrequency = pool.invoke(taskFrequencyTerm);
+//
+//            InverseDocumentFrequencyTask inverseDocumentFrequencyTask = new InverseDocumentFrequencyTask(totalTermFrequency, totalTermFrequency.size());
+//
+//            Map<String, Double> inverseTermFrequency = pool.invoke(inverseDocumentFrequencyTask);
+//
+//            TFiDFTask tFiDFTask = new TFiDFTask(totalTermFrequency, inverseTermFrequency);
+//            LOGGER.info("Term frequency task completed");
+//            List<Map<String, Double>> listTfiDf = pool.invoke(tFiDFTask);
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
         }
